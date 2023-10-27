2e4f2755d557e3b74cd683e8c9b4c4494610563a
AilsonETE <softpop.tecnologia@gmail.com>
2023-10-26 21:16:49 -0300

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'  
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) 
+
+from app import routes
