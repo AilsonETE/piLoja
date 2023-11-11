7c6f2f7ba3a5e89555ee67cc70b38a0ef79f0151
Lairson Alencar <lairson.alencar@gmail.com>
2023-11-10 21:33:03 -0300

    Ajustes para integração com o front

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 from app import app, db
 from sqlalchemy.exc import IntegrityError
-from app.models.modelos import Venda
+from app.models.modelos import Venda, Categoria
 from datetime import datetime
 
 
@@ -35,10 +35,6 @@ def get_venda(venda_id):
     })
 
 # Criar uma nova venda
-from datetime import datetime
-
-from datetime import datetime
-
 @app.route('/vendas', methods=['POST'])
 def create_venda():
     data = request.json
@@ -75,3 +71,41 @@ def delete_venda(venda_id):
     db.session.delete(venda)
     db.session.commit()
     return jsonify({'message': 'Venda excluída com sucesso'})
+
+# Listar todas as categorias
+@app.route('/categoria', methods=['GET'])
+def get_categorias():
+    categorias = Categoria.query.all()
+    
+    lista_categorias = []
+    for c in categorias:
+        lista_categorias.append({
+            'id': c.id,
+            'nome': c.nome,
+            'descricao': c.descricao,
+            'tipo': c.tipo,
+        })
+    return jsonify(lista_categorias)
+
+@app.route('/categoria', methods=['POST'])
+def create_categoria():
+    data = request.json
+    _nome = data['nome']
+    _descricao = data['descricao']
+    _tipo = data['tipo']    
+    categoria = Categoria(nome=_nome, descricao=_descricao, tipo=_tipo)
+    db.session.add(categoria)
+    db.session.commit()
+    return jsonify({'status': 201, 'message': 'Categoria criada com sucesso'}), 201
+
+
+# Excluir uma categoria
+@app.route('/categoria/<int:categoria_id>', methods=['DELETE'])
+def delete_categoria(categoria_id):
+    categoria = Categoria.query.get(categoria_id)
+    if categoria is None:
+        return jsonify({'error': 'Categoria não encontrada'}), 404
+    db.session.delete(categoria)
+    db.session.commit()
+    return jsonify({'message': 'Categoria excluída com sucesso'})
+
